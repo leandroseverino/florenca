c14e09e412df71930eeaceb0eda42ecbb4bc8e93
leandroseverino <lseverino@gmail.com>
2016-10-30 12:31:00 -0200

    corrige o bug dos statics

--- website/website/urls.py
+++ website/website/urls.py
@@ -26,3 +26,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
